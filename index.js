86f2d5e342295585f205e5c5d1c897fadf60f472
Farhan Rifaz <farhanrifaz@Farhans-MacBook-Pro.local>
2023-11-07 16:52:54 +0600

    added bookings

--- index.js
+++ index.js
@@ -16,77 +16,85 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const productCollection = client.db('tourServiceDB').collection('services')
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const serviceCollection = client.db('tourServiceDB').collection('services');
+        const bookingCollection = client.db('tourBookingDB').collection('bookings');
 
 
-    app.post('/services', async (req, res) => {
-      const newProduct = req.body
-      const result = await productCollection.insertOne(newProduct);
-      res.send(result);
-    })
 
-    app.get('/services', async (req, res) => {
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
-      res.send(result)
-    })
+        app.post('/services', async (req, res) => {
+            const newProduct = req.body
+            const result = await serviceCollection.insertOne(newProduct);
+            res.send(result);
+        })
 
-    app.get('/services/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await productCollection.findOne(query);
-      res.send(result)
-    })
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+ 
+        app.get('/bookings', async (req, res) => {
+            const cursor = bookingCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
-    app.put('/services/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updatedProduct = req.body;
-      const modProduct = {
-        $set: {
-          serviceImage: updatedProduct.serviceImage,
-          serviceName: updatedProduct.serviceName,
-          serviceProviderName: updatedProduct.serviceProviderName,
-          serviceProviderImage: updatedProduct.serviceProviderImage,
-          servicePrice: updatedProduct.servicePrice,
-          serviceArea: updatedProduct.serviceArea,
-          serviceDescription: updatedProduct.serviceDescription
-        }
-      };
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
 
-      const result = await productCollection.updateOne(filter, modProduct, options);
-      res.send(result);
-    })
+        app.put('/services/:id', async (req, res) => {
+            const id = req.params._id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedProduct = req.body;
+            const modProduct = {
+                $set: {
+                    serviceImage: updatedProduct.serviceImage,
+                    serviceName: updatedProduct.serviceName,
+                    serviceProviderName: updatedProduct.serviceProviderName,
+                    serviceProviderImage: updatedProduct.serviceProviderImage,
+                    servicePrice: updatedProduct.servicePrice,
+                    serviceArea: updatedProduct.serviceArea,
+                    serviceDescription: updatedProduct.serviceDescription
+                }
+            };
+
+            const result = await serviceCollection.updateOne(filter, modProduct, options);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-  res.send("server is running")
+    res.send("server is running")
 })
 
 app.listen(port, () => {
-  console.log(`server is running on ${port}`)
+    console.log(`server is running on ${port}`)
 })
